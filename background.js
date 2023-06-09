3c186b2ad3923d1ce6010f1d04eb4835f72ed4e3
Oliver Brew <oliver.brew2014@epsomcollege.org.uk>
2023-06-09 09:34:06 +0100

    planning

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ function getHostname(url) {
     return urlObj.hostname;
 }
 
-
+// P
 // TO DO:
 // Handling the probabilistic nature of Bloom filters (false positives and false negatives)
 // Implementing the noise-adding mechanisms for differential privacy
@@ -70,11 +70,9 @@ browser.webNavigation.onCommitted.addListener((details) => {
 browser.webRequest.onBeforeRequest.addListener(
     (details) => {
       console.log('Intercepted request to:', details.url);
-      // Here, you can add your own logic to modify the request, if needed.
-      // For example, you could change the URL:
-      // return {redirectUrl: 'https://www.example.com'};
+      // Here we will check out those sweet responses
     },
-    {urls: ['<all_urls>']},  // This will match all HTTP(S) URLs.
-    ['blocking']  // This option makes the listener blocking, which means it can modify the request.
+    {urls: ['<all_urls>']},  
+    ['blocking'] 
   );
   
