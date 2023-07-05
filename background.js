6cc3c8ca37763e5c8fe14b679acbfae6d6b394ce
Oliver Brew <oliver.brew2014@epsomcollege.org.uk>
2023-07-05 12:54:09 +0100

    crews and https checking

--- background.js
+++ background.js
@@ -1,3 +1,78 @@
+
+// Declare the primary and secondary Bloom filter variables
+let primaryBloomFilter = null;
+let secondaryBloomFilter = null;
+
+async function fetchBloomFilters() {
+    try {
+        // Fetch the primary Bloom filter
+        let response = await fetch('https://yourserver.com/primaryBloomFilter');
+
+        if (response.ok) { // if HTTP-status is 200-299
+            // get the response body
+            let json = await response.json();
+            primaryBloomFilter = new BloomFilter(json.filterSize, json.numHashes);
+            primaryBloomFilter.data = json.data;
+            console.log('Primary Bloom filter updated.');
+        } else {
+            console.error('HTTP-Error: ' + response.status);
+        }
+
+        // Fetch the secondary Bloom filter
+        response = await fetch('https://yourserver.com/secondaryBloomFilter');
+
+        if (response.ok) { // if HTTP-status is 200-299
+            // get the response body
+            let json = await response.json();
+            secondaryBloomFilter = new BloomFilter(json.filterSize, json.numHashes);
+            secondaryBloomFilter.data = json.data;
+            console.log('Secondary Bloom filter updated.');
+        } else {
+            console.error('HTTP-Error: ' + response.status);
+        }
+
+    } catch (error) {
+        console.error('Error fetching Bloom filters:', error);
+    }
+}
+
+// Call fetchBloomFilters every 2 hours
+setInterval(fetchBloomFilters, 2 * 60 * 60 * 1000);
+
+// define your values for p, q, numWebsites, primaryThresholdModifier, and secondaryThresholdModifier
+let p = 0; 
+let q = 0;
+let numWebsites = 0;
+let primaryThresholdModifier = 0;
+let secondaryThresholdModifier = 0;
+
+function reportHsts(domain) {
+    primaryBloomFilter.add(domain, p, q);
+}
+
+function reportHttp(domain) {
+    secondaryBloomFilter.add(domain, p, q);
+}
+
+function primaryTest(domain) {
+    return test(domain, primaryBloomFilter, primaryThresholdModifier);
+}
+
+function secondaryTest(domain) {
+    return test(domain, secondaryBloomFilter, secondaryThresholdModifier);
+}
+
+function test(domain, bloomFilter, thresholdMod) {
+    let count = bloomFilter.test(domain);
+    let numInsertions = bloomFilter.count;
+    
+    let adjustedCount = (count - p * numInsertions)/(q-p);
+    let threshold = (numInsertions/numWebsites) * thresholdMod;
+    return adjustedCount >= threshold;
+}
+
+// Run the fetch function once when the extension is loaded to initialize the filters
+fetchBloomFilters();
 function getHostname(url) {
     const urlObj = new URL(url);
     return urlObj.hostname;
@@ -5,45 +80,64 @@ function getHostname(url) {
 
 // P
 // TO DO:
-// Handling the probabilistic nature of Bloom filters (false positives and false negatives)
-// Implementing the noise-adding mechanisms for differential privacy
-// Communicating with the server to receive the latest Bloom filters and to submit reports
+// Handling the probabilistic nature of Bloom filters (false positives and false negatives) - DONE
+// Implementing the noise-adding mechanisms for differential privacy - READY TO ADD
+// Communicating with the server to receive the latest Bloom filters and to submit reports - DONE
 // Handling the asynchronous nature of web requests in JavaScript
 // Ensuring the security and privacy of your users
-function checkDomain(domain,bloomFilters) {
-    // Checking the domain in the primary filters
-    let primaryCounts = [bloomFilters[0].query(domain), bloomFilters[1].query(domain)];
-  
-    // If the domain is not in either of the primary filters, continue as normal
-    if (primaryCounts[0] <= 0 && primaryCounts[1] <= 0) {
-      return true // No action 
+// Global Bloom filter variables - DONE
+
+function checkHttpsLoad(url) {
+  // Create HTTPS version of the URL
+  let httpsUrl = url.replace('http://', 'https://');
+
+  // Try to fetch the HTTPS URL
+  fetch(httpsUrl, {method: 'HEAD'}).then(response => {
+    if (response.ok) {
+      console.log('HTTPS is supported:', httpsUrl);
+      return true;
     } else {
-      waitForLoad = true
-      return false // attempt https load
+      console.log('HTTPS is not supported:', httpsUrl);
+      return false;
     }
-  
-    
+  }).catch(error => {
+    console.log('Fetch failed:', error);
+    return false;
+  });
 }
 
-function checkHTTPS(domain,bloomFilters,res) {
-  // Perform check on res to see if load success
-  let httpsLoadSuccessful = check(res)
+function checkDomain(domain) {
+    // Check the domain in the primary filter
+    // let primaryCount = primaryBloomFilter.test(domain);
+  
+    // If the domain is not in the primary filter, continue as normal
+    if (primaryTest(domain)) {
+      return true // No action 
+    } else {
+      const https = checkHttpsLoad(domain)
+      return https // attempt HTTPS load
+    }
+}
+
+function checkHTTPS(domain, res) {
+  // Perform check on res to see if load successful
+  let httpsLoadSuccessful = check(res); // This check function should be defined elsewhere
   // If the page loaded successfully, no further action is required
   if (httpsLoadSuccessful) {
     console.log("HTTPS load successful");
     return 0;
   }
 
-  // If the HTTPS load was not successful, check the domain in the secondary filters
-  let secondaryCounts = [bloomFilters[2].query(domain), bloomFilters[3].query(domain)];
+  // If the HTTPS load was not successful, check the domain in the secondary filter
+  // let secondaryCount = secondaryBloomFilter.test(domain);
 
-  // If the domain is in either of the secondary filters, we have a false positive
-  if (secondaryCounts[0] > 0 || secondaryCounts[1] > 0) {
-  console.log("False positive - showing standard HTTPS-only warning");
-  return 1;
+  // If the domain is in the secondary filter, we have a false positive
+  if (secondaryTest(domain)) {
+    console.log("False positive - showing standard HTTPS-only warning");
+    return 1;
   }
 
-  // If the domain is not in the secondary filters, we assume the user is at risk
+  // If the domain is not in the secondary filter, we assume the user is at risk
   console.log("Possible MITM attack - showing custom warning");
   return 2;
 }
@@ -77,7 +171,12 @@ browser.webRequest.onBeforeRequest.addListener(
     (details) => {
       if (!waitForLoad){
         console.log('Intercepted request to:', details.url);
-        // Here we will check out those sweet responses
+        if (details.url.startsWith('http://')) {
+          console.log('Insecure request:', details.url);
+          // Optionally, you can redirect the request to HTTPS
+          let redirectUrl = details.url.replace('http://', 'https://');
+          return {redirectUrl: redirectUrl};
+        }
       }
      
     },
@@ -85,3 +184,76 @@ browser.webRequest.onBeforeRequest.addListener(
     ['blocking'] 
   );
   
+
+
+
+
+  // BloomFilter
+
+  const cryptoSubtle = window.crypto.subtle;
+
+class BloomFilter {
+  constructor(filterSize, numHashes) {
+    this.data = new Array(filterSize).fill(0);
+    this.filterSize = filterSize;
+    this.numHashes = numHashes;
+    this.count = 0;
+  }
+
+  async add(data, p, q) {
+    const [lower, upper] = await this.hashKernel(data);
+    const adq = Math.floor(q * 4294967295);
+    const adp = Math.floor(p * 4294967295);
+    const newData = new Array(this.filterSize).fill(0);
+    for (let i = 0; i < this.numHashes; i++) {
+      const trueBit = (lower + upper * i) % this.filterSize;
+      newData[trueBit] += 1;
+    }
+    let falseBits = 0;
+    for (let i = 0; i < this.filterSize; i++) {
+      const r = Math.random();
+      if (newData[i] === 1) {
+        if (r >= adq) {
+          newData[i] = 0;
+        }
+      } else {
+        if (r < adp) {
+          newData[i] = 1;
+          falseBits += 1;
+        }
+      }
+      this.data[i] += newData[i];
+    }
+    if (falseBits > 0) {
+      console.log(falseBits);
+    }
+    this.count++;
+    return this;
+  }
+
+  async test(data) {
+    const [lower, upper] = await this.hashKernel(data);
+    const result = new Array(this.numHashes);
+    for (let i = 0; i < this.numHashes; i++) {
+      const trueBit = (lower + upper * i) % this.filterSize;
+      result[i] = this.data[trueBit];
+    }
+    let min = 0;
+    for (let i = 0; i < result.length; i++) {
+      const e = result[i];
+      if (i === 0 || e < min) {
+        min = e;
+      }
+    }
+    return min;
+  }
+
+  async hashKernel(data) {
+    const encoder = new TextEncoder();
+    const hashBuffer = await cryptoSubtle.digest('SHA-256', encoder.encode(data));
+    const hashArray = Array.from(new Uint32Array(hashBuffer));
+    const upper = hashArray[0];
+    const lower = hashArray[1];
+    return [lower, upper];
+  }
+}
