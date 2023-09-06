977daccf46bfb68c3e6952478fedfb92db38fb0c
OBonk <olli.brew@outlook.com>
2023-09-06 13:07:20 +0100

    back to standard

--- background.js
+++ background.js
@@ -302,10 +302,10 @@ browser.webRequest.onHeadersReceived.addListener(
 
 browser.webRequest.onBeforeRequest.addListener(
   (details) => {
-    if (details.type === 'main_frame' && getHostname(details.url) != currentDomain){
-      console.log("ping")
-      requestStartTimes[getHostname(details.url)] = Date.now();
-    }
+    // if (details.type === 'main_frame' && getHostname(details.url) != currentDomain){
+    //   console.log("ping")
+    //   requestStartTimes[getHostname(details.url)] = Date.now();
+    // }
     if(probe==="silent" || probe==="control"){
       return
     }
@@ -498,93 +498,93 @@ hashKernel(data) {
 
 // For use when timing:
 // Function to save data to CSV from storage
-let requestStartTimes = {};
-let loopCount = 0;
-let currentSiteIndex = 0;
-let testSites = ["https://OlliBrew2.pythonanywhere.com","https://valentines.obonk.repl.co/","https://OlliBrew.pythonanywhere.com","https://list-fixing.obonk.repl.co","https://temporary-api.obonk.repl.co"]
-// For timing usage
-browser.webRequest.onCompleted.addListener(
- async (details) =>
-    {
-      console.log("pong")
-      if (details.type !== "main_frame"){
-       return; 
-      }
-      let startTime = requestStartTimes[getHostname(details.url)];
-    if (startTime) {
-      let elapsedTime = Date.now() - startTime;
-      let mode = probe === true ? "probe" : probe === false ? "silent" : "control";
-      console.log("time was: "+elapsedTime)
-      storeData(elapsedTime, mode);
-      await onTimingCompleted();
-      delete requestStartTimes[getHostname(details.url)];  // Clear recorded start time
-    }
-  },
-    {urls: ["<all_urls>"]} 
-  )
-function saveToCSV() {
-  browser.storage.local.get("requestData", function(data) {
-    let csvContent = "data:text/csv;charset=utf-8,ElapsedTime,Mode\n";
+// let requestStartTimes = {};
+// let loopCount = 0;
+// let currentSiteIndex = 0;
+// let testSites = ["https://OlliBrew2.pythonanywhere.com","https://valentines.obonk.repl.co/","https://OlliBrew.pythonanywhere.com","https://list-fixing.obonk.repl.co","https://temporary-api.obonk.repl.co"]
+// // For timing usage
+// browser.webRequest.onCompleted.addListener(
+//  async (details) =>
+//     {
+//       console.log("pong")
+//       if (details.type !== "main_frame"){
+//        return; 
+//       }
+//       let startTime = requestStartTimes[getHostname(details.url)];
+//     if (startTime) {
+//       let elapsedTime = Date.now() - startTime;
+//       let mode = probe === true ? "probe" : probe === false ? "silent" : "control";
+//       console.log("time was: "+elapsedTime)
+//       storeData(elapsedTime, mode);
+//       await onTimingCompleted();
+//       delete requestStartTimes[getHostname(details.url)];  // Clear recorded start time
+//     }
+//   },
+//     {urls: ["<all_urls>"]} 
+//   )
+// function saveToCSV() {
+//   browser.storage.local.get("requestData", function(data) {
+//     let csvContent = "data:text/csv;charset=utf-8,ElapsedTime,Mode\n";
     
-    if (data.requestData && data.requestData.length > 0) {
-      data.requestData.forEach(item => {
-        csvContent += `${item.elapsedTime},${item.mode}\n`;
-      });
+//     if (data.requestData && data.requestData.length > 0) {
+//       data.requestData.forEach(item => {
+//         csvContent += `${item.elapsedTime},${item.mode}\n`;
+//       });
 
-            // Create a blob from your CSV data:
-      let blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+//             // Create a blob from your CSV data:
+//       let blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
 
-      // Convert the blob to an Object URL:
-      let url = URL.createObjectURL(blob);
+//       // Convert the blob to an Object URL:
+//       let url = URL.createObjectURL(blob);
 
-      // Download the Object URL:
-      browser.downloads.download({
-          url: url,
-          filename: 'data.csv'
-      });
+//       // Download the Object URL:
+//       browser.downloads.download({
+//           url: url,
+//           filename: 'data.csv'
+//       });
 
-      // Revoke the Object URL after the download to free up resources:
-      browser.downloads.onChanged.addListener((delta) => {
-          if(delta.state && delta.state.current === "complete") {
-              URL.revokeObjectURL(url);
-          }
-      });
+//       // Revoke the Object URL after the download to free up resources:
+//       browser.downloads.onChanged.addListener((delta) => {
+//           if(delta.state && delta.state.current === "complete") {
+//               URL.revokeObjectURL(url);
+//           }
+//       });
 
-      browser.storage.local.remove("requestData");  // Clear stored data after saving
-    }
-  });
-}
+//       browser.storage.local.remove("requestData");  // Clear stored data after saving
+//     }
+//   });
+// }
 
-function storeData(elapsedTime, mode) {
-  browser.storage.local.get("requestData", function(data) {
-    let requestData = data.requestData || [];
-    requestData.push({ elapsedTime: elapsedTime, mode: mode });
-    browser.storage.local.set({ requestData: requestData });
-  });
-}
-async function onTimingCompleted() {
-  loopCount++;
-  console.log("------Loop count is: "+loopCount+" mode is: "+probe)
-  // If we've looped 100 times, update mode and reset counter
-  if (loopCount >= 50) {
-      console.log("100")
+// function storeData(elapsedTime, mode) {
+//   browser.storage.local.get("requestData", function(data) {
+//     let requestData = data.requestData || [];
+//     requestData.push({ elapsedTime: elapsedTime, mode: mode });
+//     browser.storage.local.set({ requestData: requestData });
+//   });
+// }
+// async function onTimingCompleted() {
+//   loopCount++;
+//   console.log("------Loop count is: "+loopCount+" mode is: "+probe)
+//   // If we've looped 100 times, update mode and reset counter
+//   if (loopCount >= 50) {
+//       console.log("100")
 
-      if (probe === 'probe') {
-          probe = "silent";
-      } else if (probe === 'silent' && !TLS) {
-          TLS = true;
-      } else if (TLS){
-        probe = "control"
-      } else if (probe === 'control') {
-          // If you have anything to do when all tests are finished, do it here
-          saveToCSV();
-          return; // end the process
-      }
-      loopCount = 0;
-  }
-  console.log("hello?")
+//       if (probe === 'probe') {
+//           probe = "silent";
+//       } else if (probe === 'silent' && !TLS) {
+//           TLS = true;
+//       } else if (TLS){
+//         probe = "control"
+//       } else if (probe === 'control') {
+//           // If you have anything to do when all tests are finished, do it here
+//           saveToCSV();
+//           return; // end the process
+//       }
+//       loopCount = 0;
+//   }
+//   console.log("hello?")
 
-  // Move to the next site and open in a new tab
-  currentSiteIndex = (currentSiteIndex + 1) % testSites.length;
-  browser.tabs.update({ url: testSites[currentSiteIndex] });
-}
+//   // Move to the next site and open in a new tab
+//   currentSiteIndex = (currentSiteIndex + 1) % testSites.length;
+//   browser.tabs.update({ url: testSites[currentSiteIndex] });
+// }
